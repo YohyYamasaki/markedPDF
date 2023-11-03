ee2d6c80106aac4d2115a15a805db3bc039f6461
Yohei <74522538+YohyYamasaki@users.noreply.github.com>
2023-11-03 23:12:54 +0100

    change vite setting to avoid memory error

--- html/react/vite.config.ts
+++ html/react/vite.config.ts
@@ -1,13 +1,16 @@
 import {defineConfig, loadEnv} from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({mode}) =>{
-  const env = loadEnv(mode, process.cwd(), '');
+// メモリ制限を増やす
+process.env.NODE_OPTIONS = "--max-old-space-size=1024";
+
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode, process.cwd(), "");
 
   return {
     plugins: [react()],
     define: {
-      'process.env.REACT_APP_ENV_URL': JSON.stringify(env.REACT_APP_ENV_URL)
+      "process.env.REACT_APP_ENV_URL": JSON.stringify(env.REACT_APP_ENV_URL),
     },
     base: "/markedPDF/",
     server: {
